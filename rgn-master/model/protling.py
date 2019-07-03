9419410b48809bb966f5dce9979e70474e9e01b8
Eleonora <eleonora.shantsila@gmail.com>
2019-07-03 10:48:38 +0100

    rgn new

--- rgn-master/model/protling.py
+++ rgn-master/model/protling.py
@@ -21,7 +21,7 @@ from itertools import izip_longest
 # constant directory and file names
 RUNS_DIRNAME = 'runs'
 DATAS_DIRNAME = 'data'
-CHECKPOINTS_DIRNAME = 'checkpoints'
+CHECKPOINTS_DIRNAME = 'checkpoint'
 LOGS_DIRNAME = 'logs'
 ALPHABETS_DIRNAME = 'alphabets'
 FULL_TRAINING_DIRNAME     = 'training'
